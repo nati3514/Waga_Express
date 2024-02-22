84917561db3fbfb1cbb98cfb4d9ad275413a24e6
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-22 16:40:57 +0300

    Update about.blade.php

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -4,9 +4,7 @@
 <div class="container-fluid py-5 mt-5">
     <div class="container">
         <div class="row g-5">
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                
-            </div>
+           
             
         </div>
     </div>
