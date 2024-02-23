5269ff9b242991bfdc8c52e9886b05096719369b
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-23 16:46:22 +0300

    Update StaffController.php

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -1,1 +1,15 @@
 
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\WeightPrice;
+use App\Notifications\staffNotification;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
