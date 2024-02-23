17a2cc7542f34aa9dca63a604a046b007b14504f
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-23 16:46:53 +0300

    Update StaffController.php

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -13,3 +13,17 @@ use App\Notifications\staffNotification;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
+class StaffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    function __construct()
+    {
+        $this->middleware('role_or_permission:Staff access|Staff add|Staff edit|Staff delete', ['only' => ['index','show']]);
+        $this->middleware('role_or_permission:Staff add', ['only' => ['create','store']]);
+        $this->middleware('role_or_permission:Staff edit', ['only' => ['edit','update']]);
+        $this->middleware('role_or_permission:Staff delete', ['only' => ['destroy']]);
+        
+    }
+    
