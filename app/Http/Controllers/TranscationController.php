fb6e438e12370bec35b808f64d2dbe2658331b7f
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-23 16:51:11 +0300

    Update TranscationController.php

--- app/Http/Controllers/TranscationController.php
+++ app/Http/Controllers/TranscationController.php
@@ -14,3 +14,17 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Notification;
 use Spatie\Permission\Models\Role;
+class TranscationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+     public function reports(Request $request){
+
+        $user = Auth::user();
+    //     $usersWithSameBranch = User::where('branch_id', $user->branch_Id)
+    // ->get(['first_name']);
+
+
+    //     dd($usersWithSameBranch);
