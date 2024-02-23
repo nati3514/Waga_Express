047d5a85617b373645f4300d3730d2f0963fbfe7
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-23 16:50:54 +0300

    Update TranscationController.php

--- app/Http/Controllers/TranscationController.php
+++ app/Http/Controllers/TranscationController.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use App\Models\branch;
+use App\Models\StaffTransaction;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use App\Models\User as ModelsUser;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Transaction;
+use App\Notifications\depositNotification;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Notification;
+use Spatie\Permission\Models\Role;
