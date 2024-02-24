003f0328aa80fb969f064382342ca0045d779c12
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-24 16:10:33 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,12 +33,7 @@ class DatabaseSeeder extends Seeder
                 
                 
             
-          
-                'Tot_package' => '10',
-                'balance' => 20000,
-                'Tot_commission' => 200,
-                'email' => 'branch2@gmail.com',
-                'started_date' => '2021-05-09',
+         
                 'branch_name' => 'Gonder Branch',
             ]);
             branch::create([
