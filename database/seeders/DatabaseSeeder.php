9b154cf81f0d85f1f852a4abc095d0660ffa70ec
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-24 16:12:30 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,10 +34,7 @@ class DatabaseSeeder extends Seeder
               
 
           
-                'country' => 'Ethiopia',
-                'contact' => '+25166941',
-                'Tot_package' => '10',
-                'balance' => 50000,
+                
                 'Tot_commission' => 200,
                 'email' => 'branch2@gmail.com',
                 'started_date' => '2021-05-09',
