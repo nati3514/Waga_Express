08df7b67bec6aa0d5ab2fdd00ad8303dc6db7127
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-25 14:20:59 +0300

    Update team.blade.php

--- resources/views/frontend/team.blade.php
+++ resources/views/frontend/team.blade.php
@@ -7,12 +7,7 @@
         <div class="row g-4">
             
             
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="team-item">
-                    
-                    
-                </div>
-            </div>
+            
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                 <div class="team-item">
                     <div class="position-relative overflow-hidden">
