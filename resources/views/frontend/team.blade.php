c3125860ecb3118aca2e8176787d287a711febfb
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-25 14:15:47 +0300

    Update team.blade.php

--- resources/views/frontend/team.blade.php
+++ resources/views/frontend/team.blade.php
@@ -6,11 +6,7 @@
         </div>
         <div class="row g-4">
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="team-item">
-                    
-                        
-                            
-                    </div>
+                
                     <div class="text-center p-4">
                         <h5 class="mb-1">Nati Age</h5>
                         <span>Programmer</span>
