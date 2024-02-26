2da9c11a9b9ad11956d2290da619effcd5d9ba77
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-26 16:32:19 +0300

    Update brand.blade.php

--- resources/views/frontend/brand.blade.php
+++ resources/views/frontend/brand.blade.php
@@ -3,10 +3,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <div class="d-flex">
                 <i class="bi bi-telephone-inbound fs-2"></i>
-                <div class="ms-3">
-                    
-                   
-                </div>
+                
             </div>
             
             
