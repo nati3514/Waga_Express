8a0d6126e7548ff50eb2ea6539994ca016b3f6c2
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-26 16:33:08 +0300

    Update videomodal.blade.php

--- resources/views/frontend/videomodal.blade.php
+++ resources/views/frontend/videomodal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content rounded-0">
             <div class="modal-header">
                 <h3 class="modal-title" id="exampleModalLabel">Youtube Video</h3>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                
             </div>
            
         </div>
