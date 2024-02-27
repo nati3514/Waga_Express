cfd421c2da9ecd134855239b73fe0177b75c4ae5
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-27 15:12:24 +0300

    Update services.blade.php

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -9,9 +9,7 @@
            
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                 <div class="service-item">
-                    <div class="icon-box-primary mb-4">
-                        
-                    </div>
+                    
                   
                    
                   
