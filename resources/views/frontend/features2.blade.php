941177927a2d730f9ffa642340b930817e8f1efd
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-29 19:07:53 +0300

    Update features2.blade.php

--- resources/views/frontend/features2.blade.php
+++ resources/views/frontend/features2.blade.php
@@ -5,12 +5,7 @@
             <div class="col-lg-6">
                 <div class="row h-100 align-items-end">
                     <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
-                        <div class="d-flex align-items-center justify-content-center" style="min-height: 300px;">
-                            {{-- <button type="button" class="btn-play" data-bs-toggle="modal"
-                                data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
-                                <span></span>
-                            </button> --}}
-                        </div>
+                        
                     </div>
 
                 </div>
