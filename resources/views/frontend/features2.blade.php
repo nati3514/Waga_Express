c41333d69580e83abcbdd2ebe74f6a4613ed7cba
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-02-29 19:07:31 +0300

    Update features2.blade.php

--- resources/views/frontend/features2.blade.php
+++ resources/views/frontend/features2.blade.php
@@ -1,13 +1,7 @@
 <div class="container-fluid feature mt-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container">
         <div class="row g-0">
-            <div class="col-lg-6 pt-lg-5">
-                
-                        
-                    </div>
-                    
-                </div>
-            </div>
+            
             <div class="col-lg-6">
                 <div class="row h-100 align-items-end">
                     <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
