739b2a5b30c862e5df62fafce2dcb94d067f276d
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-03-06 17:33:17 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
             
         If(Auth::user()->hasRole ('admin')) {
             if ($data) {
-                $dataId = $data->id;
+                
 
 
                 $currentDate = Carbon::now()->format('Y-m-d');
