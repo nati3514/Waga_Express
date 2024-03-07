2f28c3e054195c20adc590fca77115f076e79375
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-03-07 11:34:19 +0300

    Update 2013_10_27_103948_create_branches_table.php

--- database/migrations/2013_10_27_103948_create_branches_table.php
+++ database/migrations/2013_10_27_103948_create_branches_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('state');
             $table->string('country');
             $table->string('contact');
-            $table->string('Tot_Package');
+            
             
             
             
