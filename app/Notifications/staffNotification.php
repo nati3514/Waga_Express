445436a5e5e9f1612ec050ae33d5d5dc53fd0e6c
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-03-13 16:47:50 +0300

    Update staffNotification.php

--- app/Notifications/staffNotification.php
+++ app/Notifications/staffNotification.php
@@ -50,10 +50,6 @@ class staffNotification extends Notification
      */
     public function toArray(object $notifiable): array
     {
-        return [
-            
-            
-          
-        ];
+        
     }
 }
