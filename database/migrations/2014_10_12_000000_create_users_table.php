de09fd6e375104885513d55c10311415c2988c8c
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-03-13 16:50:25 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('status');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreign('branch_Id')->references('id')->on('branches');
+           
             
        
         });
