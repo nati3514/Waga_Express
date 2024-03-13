8dbe18dbfe865c734133f08218e01eed7f6364f5
Natnael Agenagnew <103646926+nati3514@users.noreply.github.com>
2024-03-13 16:51:42 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('amount_limit');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+           
             
           
            
